f47d9a1f0bb210c1ba0afd051a74e53574bec847
Shyaka <tshyaka6@gmail.com>
2025-02-07 15:56:09 +0200

    This shows the no number values

--- mypro.py
+++ mypro.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pandas import DataFrame
+import numpy as np
+
 data=DataFrame({'Name':['SHyaka','Thierry','Manzi'],
-                'Age':[25,25,34]})
+                'Age':[25,25,pd.nan]})
 print(data)
