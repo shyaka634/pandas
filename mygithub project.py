3373b687f58475077305a518b6e2e5e81a3d64ce
Shyaka <tshyaka6@gmail.com>
2025-02-09 19:58:52 +0200

    This my new branch

--- mygithub project.py
+++ mygithub project.py
@@ -1,10 +1,1 @@
-import pandas as pd
-from pandas import DataFrame
-import numpy as np
-
-data=DataFrame({'Names':['Shyaka',None,'Manzi'],
-                'Age':[12,np.nan,13]})
-data['Names']=data['Names'].fillna('Unknown')
-data['Age']=data['Age'].fillna('invalid')
-print(data)
 
--- mypro.py
+++ mypro.py
@@ -1,7 +1,15 @@
-import pandas as pd
-from pandas import DataFrame
-import numpy as np
+import matplotlib.pyplot as plt
 
-data=DataFrame({'Name':['SHyaka','Thierry','Manzi'],
-                'Age':[25,25,pd.nan]})
-print(data)
+x = [0, 2, 4, 6, 8]
+y = [0, 4, 16, 36, 64]
+
+fig, ax = plt.subplots()  
+ax.plot(x, y, marker='o', label="Data Points")
+
+ax.set_title("Basic Components of Matplotlib Figure")
+ax.set_xlabel("X-Axis") 
+ax.set_ylabel("Y-Axis")  
+
+
+plt.show()
+
